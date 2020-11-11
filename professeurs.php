eb00f2eb60381e7e735dc8fb0729384ea89e3f1a
JoelTremblay <44471300+JoelTremblay@users.noreply.github.com>
2020-11-11 09:54:26 -0500

    Update galerie photo

--- professeurs.php
+++ professeurs.php
@@ -7,22 +7,67 @@
         <section>
           <div class="galerie">
             <a href="#" class="item">
-              <img src="medias/images/dom.png" alt="Dominic">
+              <img src="medias/images/dom.jpg" alt="Dominic">
+              <div class="overlay"></div>
             </a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
-            <a href="#" class="item"></a>
+            <a href="#" class="item">
+              <img src="medias/images/camille.jpg" alt="Camille">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/caroline.jpg" alt="Dominic">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/denis.jpg" alt="Dominic">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/vahik.jpg" alt="Dominic">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/vincent.jpg" alt="Camille">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/eddy.jpg" alt="Dominic">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/greg.jpg" alt="Dominic">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/johanne-gd.jpg" alt="Dominic">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/jonathan.jpg" alt="Camille">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/manon.jpg" alt="Dominic" class="adjust">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/martin.jpg" alt="Dominic">
+              <div class="overlay"></div>
+            </a>
+            <a href="#" class="item">
+              <img src="medias/images/mathieu.jpg" alt="Dominic">
+              <div class="overlay"></div>
+            </a>
+            <a class="item texte">
+              <div class="centre">
+                <p>Voici</p>
+                <p>les</p>
+                <p>professeurs!</p>
+              </div>
+            </a>
+            <a href="#" class="item">
+              
+            </a>      
         </div>
         </section>
 <?php
