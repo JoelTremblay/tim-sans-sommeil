bd681a0305e780d2b18d5960a8e1d18d6d35f1fc
Diphany <diphany.chea@gmail.com>
2020-12-10 15:17:00 -0500

    Nouveaux fonts
    Implémentation des typographies

--- Header-navigation.php
+++ Header-navigation.php
@@ -9,6 +9,9 @@
         <meta name="author" content="SansSommeil">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="stylesheet" href="sass/initialize.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poiret+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@700&display=swap" rel="stylesheet">
         <?php 
             if ($page == "accueil") echo'<link rel="stylesheet" href="sass/index.css">';
             else if ($page == "grilleCours")
