66c708bf1b40bd42e5c06a9640d34e5100f26e13
Diphany <diphany.chea@gmail.com>
2020-12-14 08:11:17 -0500

    Remplacement des logos/médias
    Changement des logos et des médias + correction CSS

--- stages.php
+++ stages.php
@@ -8,13 +8,13 @@
   <section class="entete-stages">
     <h1>Stages en milieu professionel</h1>
     <div class="arriere-plan">
-      <img class="img-entete" src="medias/images/banniere-entreprise.png" alt="">
+      <!-- <img class="img-entete" src="medias/images/banniere-entreprise.png" alt=""> -->
     </div>
   </section>
   <section class="categories">
     <ul>
       <li><a href="stages-art.php">Artistique</a><img class="img-categorie" src="../tim-sans-sommeil/medias/images/art.png" alt=""></li>
-      <li><a href="stages-prog.php">Programmation</a><img class="img-categorie" src="../tim-sans-sommeil/medias/images/prog.png" alt=""></li>  
+      <li><a href="stages-prog.php">Programmation</a><img class="img-categorie" src="../tim-sans-sommeil/medias/images/prog-logo.png" alt=""></li>  
     </ul>
     <img class="ligne-pattern1" src="medias/images/lignes-pattern-stages.png" alt="">
     <img class="ligne-pattern2" src="medias/images/lignes-pattern-stages.png" alt="">
