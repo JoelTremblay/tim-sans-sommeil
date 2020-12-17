18ff6c04348096ccbef225495fb81200219a1c1b
Diphany <diphany.chea@gmail.com>
2020-12-17 08:06:55 -0500

    Layout corrigé
    Retouchement du SCSS des layout des pages complétées

--- Header-navigation.php
+++ Header-navigation.php
@@ -91,9 +91,9 @@
                                     else if ($page == "etudiants") echo' ';
                                     else if ($page == "professeurs") echo' ';
                                     else if ($page == "projets") echo' ';
-                                    else if ($page == "stages") echo'Programation';
-                                    else if ($page == "stages-art") echo'Programation';
-                                    else if ($page == "stages-prog") echo'Programation';
+                                    else if ($page == "stages") echo'Programmation';
+                                    else if ($page == "stages-art") echo'Programmation';
+                                    else if ($page == "stages-prog") echo'Programmation';
                                 ?>
                         </a>
                     </li>
